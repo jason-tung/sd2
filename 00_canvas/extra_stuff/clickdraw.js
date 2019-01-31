68a0fd67c9c48189414821e127d440dfee9325a4
Jason <mickeu420@gmail.com>
2019-01-31 13:14:45 -0500

    removed extraneous calvo and refactored getting mosue coords with offset instead of canvasboudning rect

--- 00_canvas/extra_stuff/clickdraw.js
+++ 00_canvas/extra_stuff/clickdraw.js
@@ -13,10 +13,16 @@ var h = c.getAttribute("height");
 var ctx = c.getContext("2d");
 var cbtn = document.getElementById("clear");
 var tbtn = document.getElementById("toggle");
+var mode = document.getElementById("mode");
 
-//set the fill colors now
-ctx.fillStyle = "black";
-ctx.strokeStyle = "black";
+//fx to get real coord of mouse as a json
+function getMousePos(canvas, e) {
+    var rect = canvas.getBoundingClientRect();
+    return {
+        x: e.clientX - rect.left,
+        y: e.clientY - rect.top
+    };
+}
 
 //add listened to clear button to wipe the page
 cbtn.addEventListener('click', function () {
@@ -26,17 +32,19 @@ cbtn.addEventListener('click', function () {
 //adds event listener to toggle button to swap the state vars
 tbtn.addEventListener('click', function () {
     rect ^= true;
+    mode.innerHTML = rect ? "rectangle" : "dot"
 });
 
 //adds event listener to canvas to draw on it when theres a click
 c.addEventListener('click', function (e) {
-    var cds = {"x":e.offsetX, "y":e.offsetY};
+    var cds = getMousePos(c, e);
+    var r_color = getRandomColor();
+    ctx.fillStyle = r_color;
     if (rect) {
-	//draw a 100 width 200 height rect at the mousex mousey
         ctx.fillRect(cds.x, cds.y, 100, 200);
     }
     else {
-	//draw a radius 10 circle at mousex mousey
+        ctx.strokeStyle = r_color;
         ctx.beginPath();
         ctx.ellipse(cds.x, cds.y, 10, 10, 0, 0, 2 * Math.PI);
         ctx.stroke();
