8835ca83de21678d2aadf5a278104c60c209c684
Jason Tung <mickeu420@gmail.com>
2019-03-04 20:46:21 -0500

    kk

--- 07_mongo/main.py
+++ 07_mongo/main.py
@@ -11,7 +11,9 @@
 # CONTAINS INFORMATION FOR EACH POKEMON IN POKEMON GO
 # NOTE: WE ONLY PARSED INFORMATION PERTAINING TO REAL POKEMON GAMES!!!
 # LINK: https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json
-# IMPORT MECHANISM: TOOK THE LIST INSIDE OF "pokemon" AND GENERATED THE JSON WITH THAT INSTEAD!!! FILE IMPORTED FOR PROJECT: POKEDEX_PARSED.JSON
+# IMPORT MECHANISM: parsed pokedex.json from db.pokedex_unparsed in json_setup.pymongo
+# imported pokedex_parsed.json as db.azrael.
+
 
 import pymongo
 
@@ -20,23 +22,33 @@ connection = pymongo.MongoClient(server_addr)
 db = connection.test
 connection = db.azrael
 
-ary_len = len(connection.find_one({})["pokemon"])
+
+# for k in connection.find({}):
+#     print(k)
+
 
 def find_pokemans(**kwargs):
-    #POSSIBLE ARGS: num, name, type, height, height_updown, weight, weight_updown, weaknesses, evolutions
-    ntl_num = kwargs.get("num", None)
-    name = kwargs.get("name", None)
-    pk_type = kwargs.get("type", None)
-    height = kwargs.get("height", None)
-    height_updown = kwargs.get("height_updown", "up")
-    weight = kwargs.get("weight", None)
-    weight_updown = kwargs.get("weight_updown", "up")
-    weaknesses = kwargs.get("weaknesses", None)
-    evolutions = kwargs.get("type", None)
+    # POSSIBLE ARGS: num, name, type, height, height_updown, weight, weight_updown, weaknesses, evolutions
     find_query = {}
+    args = {"num": None, "name": None, "type": None, "height": None,
+            "weight": None, "weaknesses": None, "evolutions": None}
+    for k in args.keys():
+        args[k] = kwargs.get(k, None)
+        if args[k] is not None:
+            if k == "evolutions":
+                find_query["$or"] = [{"next_evolution": {"$elemMatch": {"name": args[k]}}},
+                                     {"prev_evolution": {"$elemMatch": {"name": args[k]}}}]
+            elif k in ["weaknesses", 'type']:
+                find_query[k] = {'$in': args[k]}
+            else:
+                find_query[k] = args[k]
+    print(find_query)
+    for k in connection.find(find_query):
+        print(k)
 
-    
 
-
-
-
+find_pokemans(evolutions="Charizard")
+print("-------------")
+find_pokemans(height='1.19 m', type=['Flying'])
+print("-------------")
+find_pokemans(num={"$gt": "050"}, height={"$gt": "1.00 m"}, type=['Water', 'Ice'], weaknesses=['Bug', 'Fire'])
