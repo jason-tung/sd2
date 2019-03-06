2f2ce5161c9b6d25ec28a9d88c82b020fc6b9799
Jason Tung <mickeu420@gmail.com>
2019-03-05 19:53:18 -0500

    fixed

--- 07_mongo/json_setup.py
+++ 07_mongo/json_setup.py
@@ -14,16 +14,22 @@ connection = pymongo.MongoClient(server_addr)
 db = connection.test
 connection = db.pokedex_unparsed
 
+id = 1
 pkmon_list = connection.find_one({})["pokemon"]
 with open("pokedex_parsed.json", "w") as f:
+    f.write("[")
     for pkmon in pkmon_list:
         f.write(json.dumps(pkmon))
-        f.write("\n")
+        if id < 151:
+            f.write(",\n")
+        id+=1
+    f.write("]")
 
 collection = db.azrael
-        
+collection.drop()
 f=open("pokedex_parsed.json","r")
-data=json.loads(f.read())
+json_data = f.read()
 f.close()
+data = json.loads(json_data)
 
 collection.insert_many(data)
