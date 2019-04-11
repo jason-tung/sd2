209169bf5a4daab9cc56c349579decca9e4603e6
Jason Tung <mickeu420@gmail.com>
2019-04-11 15:50:48 -0400

    .,/[][]\\

--- 16_something/main.py
+++ 16_something/main.py
@@ -5,7 +5,9 @@ def genpw():
     return ''.join([chr((ord('A') if randint(0,1) == 0 else ord('a')) + randint(0,25)) if randint(0,2) == 0 else str(randint(0,10)) for x in range(20)])
 
 def runmen(lu):
-    return sum([1 if x in [chr(ord('a') + lower) for lower in range(26)] else (2 if x in [chr(ord('A') + upper) for upper in range(26)] else (3 if ord(x) <= 9 else 5)) for x in lu])
+    return sum([1 if ord('a') <= ord(x) <= ord('z') else (2 if ord('A') <= ord(x) <= ord('Z') else (3 if ord(x) <= 9 else 5)) for x in lu]) + 8 if sum([0 if x.isalnum() else 1 for x in lu]) > 0 else 0
+
+
 
 print(genpw())
-print(runmen("quadrakill1234"))
+print(runmen("quadrakill!!1234"))
