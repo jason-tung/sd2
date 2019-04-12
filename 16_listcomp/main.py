ac2a8ab3df1d6864a2f712f64a1a44dc07996edb
Jason Tung <mickeu420@gmail.com>
2019-04-11 21:45:40 -0400

    mastermind...

--- 16_listcomp/main.py
+++ 16_listcomp/main.py
@@ -1,20 +1,14 @@
+import functools
 from random import randint
 
 
-def genpw():
-    return ''.join([chr((ord('A') if randint(0,1) == 0 else ord('a')) + randint(0,25)) if randint(0,2) == 0 else str(randint(0,10)) for x in range(20)])
-
-def genpw2():
-    return ''.join([chr(randint(30,126)) for x in range(20)])
+def check_critera(st):
+    return functools.reduce(lambda x,y: x * y, [2 if x.isupper() else (3 if x.islower() else 5 if x.isdigit() else 1)for x in st] )%(2*3*5) == 0
 
 def runmen(lu):
     return 10 - 10/(sum([1 if x.islower() else (2 if x.isupper() else (3 if x.isdigit() else 5)) for x in lu]) + (8 if any([not x.isalnum() for x in lu]) else 0) + (4 if any([x.isnumeric() for x in lu]) else 0))**.35
 
-print(genpw())
-print(genpw2())
-print(runmen("dog"))
-print(runmen("avvv"))
-print(runmen("password"))
-print(runmen("AAAAAbbbbbb888900"))
-print(runmen("AAAAAbbbbbb888900!"))
-print(runmen("AAAAAbbbbbb888900!!"))
+passwords = ["dog", "avVVvvvv", "password", "AAb888900", "AAb888900!", "AAAAAbbbbbb888900!!"]
+for x in passwords:
+    print(check_critera(x))
+    print(runmen(x))
