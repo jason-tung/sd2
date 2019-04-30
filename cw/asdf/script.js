18aaef29cad41afdc6833df2c60c3255a9bedb82
Jason <mickeu420@gmail.com>
2019-04-30 03:09:41 -0400

    jfedcak

---
+++ cw/asdf/script.js
@@ -0,0 +1,8 @@
+var stuff = document.getElementById("stuff");
+
+jQuery.getJSON("https://raw.githubusercontent.com/stuy-softdev/workshop/master/thluffy/21_js-mfr/2006_-_2012_School_Demographics_and_Accountability_Snapshot.json?token=AH4SWOZPZPEXHEXK2NQX44C42BTIU", function(data){
+    var gender_map = data.
+    var gender_count = ;
+    var male_count = 0;
+    var female_count = 0;
+});
