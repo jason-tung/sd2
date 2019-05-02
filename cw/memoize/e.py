8d087af655763c3b6ba5efe0addf49284b6ffe7b
Jason Tung <mickeu420@gmail.com>
2019-05-01 21:38:55 -0400

    dsakf

--- cw/memoize/e.py
+++ cw/memoize/e.py
@@ -1,3 +1,21 @@
+#
+# def memoize(f):
+#     hello = {}
+#     def inner(x):
+#         if x not in hello:
+#             hello[x] = f(x)
+#         return hello[x]
+#     return inner
+#
+# def fib(n):
+#     if n < 2:
+#         return n
+#     return fib(n-1) + fib(n-2)
+#
+# fib = memoize(fib)
+# print(fib(52))
+
+
 
 def memoize(f):
     hello = {}
@@ -7,10 +25,10 @@ def memoize(f):
         return hello[x]
     return inner
 
+@memoize
 def fib(n):
     if n < 2:
         return n
     return fib(n-1) + fib(n-2)
 
-fib = memoize(fib)
 print(fib(52))
