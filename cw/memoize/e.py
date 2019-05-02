3655edb67b060c836968335112b93582a8faf82f
Jason Tung <mickeu420@gmail.com>
2019-05-01 21:35:36 -0400

    jj

---
+++ cw/memoize/e.py
@@ -0,0 +1,16 @@
+
+def memoize(f):
+    hello = {}
+    def inner(x):
+        if x not in hello:
+            hello[x] = f(x)
+        return hello[x]
+    return inner
+
+def fib(n):
+    if n < 2:
+        return n
+    return fib(n-1) + fib(n-2)
+
+fib = memoize(fib)
+print(fib(52))
